8fe2fe3f23f8ffb02a6ece360c980d046aa2eff5
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-04-25 17:20:59 +0900

    peer

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white max-w-screen-sm flex flex-col md:flex-row gap-2 w-full shadow-lg p-5 rounded-3xl">
         <input
-          type="text"
-          placeholder="Search"
-          className="w-full rounded-full p-3 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow "
+          type="email"
+          placeholder="Email address"
+          required
+          className="w-full rounded-full p-3 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
         />
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className=" bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none md:px-10">
+          Log in
         </button>
       </div>
     </main>
