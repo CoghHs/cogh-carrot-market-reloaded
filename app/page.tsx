518e82538d04bc62b390d80820168506b4504878
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-04-27 14:49:32 +0900

    animate

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white max-w-screen-sm flex flex-col md:flex-row gap-2 w-full shadow-lg p-5 rounded-3xl *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-200 ">
-        <input
-          type="email"
-          placeholder="Email address"
-          required
-          className="w-full rounded-full p-3 bg-gray-200 pl-5  ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className=" bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium  md:px-10">
-          Log in
-        </button>
+      <div className="bg-white max-w-screen-sm flex flex-col gap-3 w-full shadow-lg p-5 rounded-3xl ">
+        {["Nico", "Cogh", "Zu", "Jake"].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 ">
+            <div className="size-7 bg-blue-400 rounded-full" />
+            <span className=" text-lg font-medium">{person}</span>
+            <div className="relative size-7 bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span className="z-10">{index}</span>
+              <div className="size-7 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
