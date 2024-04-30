da9cdb52171754286d2ac38e20f4f701c6361e64
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-04-30 17:34:07 +0900

    ui css

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white max-w-screen-sm flex flex-col gap-3 w-full shadow-lg p-5 rounded-3xl ">
-        <div className="group flex flex-col">
-          <input
-            className="w-full bg-gray-100"
-            type="email"
-            placeholder="Write your email"
-          />
-          <span className="group-focus-within:block hidden">
-            Make sure it is a valid email
-          </span>
-          <button>Submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6 ">
+      <div className="my-auto *:font-medium flex flex-col items-center gap-2">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
+      </div>
+      <div className="flex flex-col gap-3 items-center w-full">
+        <Link className="primary-btn py-2.5 text-lg" href="/create-account">
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
