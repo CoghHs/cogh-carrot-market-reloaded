56068eb11c5b2ce6f7754a7e9f01a3946759b9f5
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-05-14 17:53:43 +0900

    i iron-session

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -6,6 +6,7 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { boolean, z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -80,5 +81,17 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
   }
 }
