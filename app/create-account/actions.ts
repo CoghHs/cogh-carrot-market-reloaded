79754755c093fa47035017c3e26e18beb0182f29
박형석 <cogh@baghyeongseog-ui-MacBookPro.local>
2024-05-14 17:24:59 +0900

    update zod

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,7 +4,8 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
-import { z } from "zod";
+import db from "@/lib/db";
+import { boolean, z } from "zod";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -15,6 +16,31 @@ const checkPassword = ({
   password: string;
   confirmPassword: string;
 }) => password === confirmPassword;
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -24,12 +50,18 @@ const formSchema = z
       })
       .toLowerCase()
       .trim()
-      .refine(checkUsername, "No potatoes allowed"),
-    email: z.string().email().toLowerCase(),
-    password: z
+      .refine(checkUsername, "No potatoes allowed")
+      .refine(checkUniqueUsername, "This Username is already taken."),
+    email: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+      .email()
+      .toLowerCase()
+      .refine(
+        checkUniqueEmail,
+        "There is an account already registered with that email."
+      ),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR)
     confirmPassword: z.string().min(PASSWORD_MIN_LENGTH),
   })
   .refine(checkPassword, {
@@ -44,10 +76,9 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
   }
 }
